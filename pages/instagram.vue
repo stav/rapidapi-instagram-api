ae2b32bc4e4fbb68f945cd09dca0f44d080babe3
Steven Almeroth <sroth77@gmail.com>
2020-08-14 18:40:39 -0400

    template

---
+++ pages/instagram.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <v-icon
+        size="100pt"
+      >
+        mdi-instagram
+      </v-icon>
+    </v-flex>
+  </v-layout>
+</template>
