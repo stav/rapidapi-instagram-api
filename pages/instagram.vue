191c99ae94b5bf24d8593fe07b65397e5ff0f6fb
Steven Almeroth <sroth77@gmail.com>
2020-08-17 13:12:56 -0400

    real data
    but no api key

--- pages/instagram.vue
+++ pages/instagram.vue
@@ -18,11 +18,18 @@
             X
           </v-btn>
           <v-btn
-            title="Go get the data"
+            title="Go get the fake data"
             color="primary"
             @click="getInstagramFakeData"
           >
             Fake
+          </v-btn>
+          <v-btn
+            title="Go get the real data"
+            color="success"
+            @click="getInstagramAPIData"
+          >
+            Fetch
           </v-btn>
         </v-card-actions>
         <v-card-text>
@@ -53,6 +60,22 @@ export default {
     },
     getInstagramFakeData () {
       this.result = instagram
+    },
+    async getInstagramAPIData () {
+      const query = {
+        short_code: 'BzinsTanXXv', // use any shortcode you want
+      }
+
+      const url = new URL('https://instagram28.p.rapidapi.com/media_likers')
+      url.search = new URLSearchParams(query).toString()
+
+      const headers = {
+        'x-rapidapi-host': 'instagram28.p.rapidapi.com',
+        'x-rapidapi-key': '[PUT YOUR API KEY HERE]',
+      }
+
+      const response = await fetch(url.toString(), { headers })
+      this.result = await response.json()
     },
   },
 }
